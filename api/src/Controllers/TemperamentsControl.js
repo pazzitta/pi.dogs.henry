9ea53d98e07d3d469b86eb2e3d719ed5540ca81f
Your Name <you@example.com>
2022-03-15 09:48:38 -0300

    temperaments almost ok

--- api/src/Controllers/TemperamentsControl.js
+++ api/src/Controllers/TemperamentsControl.js
@@ -2,8 +2,46 @@ const axios = require ('axios');
 const {Dog, Temperament} = require ('../db')
 const {API_KEY1} = process.env
 
-const getAllTemperaments = () => {
 
+const allTemRep = async () => {
+    try {
+        const pedidoApiAll = await axios (`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY1}`);
+        const dataApiAll = await pedidoApiAll.data;
+    
+        if (dataApiAll){
+            let infoApiTemp = dataApiAll?.map (el => {
+                const todosJuntos = el.temperament
+                return (todosJuntos)
+            })
+            return (infoApiTemp)
+        }
+    } catch (error) {
+        console.log ('Error en getAllDogsApi')
+  }
+}
+
+const allInArray = async () => {
+    try {
+    let auxiliar = [];
+    const arrayOfObjects = await allTemRep ();
+
+    for (i = 0; i < arrayOfObjects.length; i ++) {
+        auxiliar.push(i);
+    } return (auxiliar)
+
+    }catch (error) {
+        console.log ('no anda allInArray')
+    }
+}
+
+const getAllTemperaments = async (req, res, netx) => {
+  try {
+    const vertem = await allTemRep();
+    res.send (vertem)
+  } catch (error) {
+      console.log ('no anda getAllTemperaments')
+  }
+    
 };
 
 
